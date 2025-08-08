ca301d584e8badd24395f1f8f41a7f30a7b7fc83
piyushprajapati52 <prajapatipuso1976@gmail.com>
2025-08-08 09:28:07 +0530

    Update sort sorted.py

--- module 7   Python – Collections, functions and Modules/practical/3. Working with Lists/sort sorted.py
+++ module 7   Python – Collections, functions and Modules/practical/3. Working with Lists/sort sorted.py
@@ -1,16 +1,6 @@
 # Original list
 
 
-number = [5,2,6,4,8,9]
-
-
-# Using sorted() - returns a new sorted list (original list remains unchanged)
-
-sorted_number = sorted(number)
-print("Using sorted:", sorted_number)
-print("Original list after sorted:", number)
-
-# Using sort() - sorts the list in-place (modifies the original list)
+number = [5, 2, 6, 4, 8, 9]
 number.sort()
-number.sort()
-print("Using sort:", number)
+print(number)
