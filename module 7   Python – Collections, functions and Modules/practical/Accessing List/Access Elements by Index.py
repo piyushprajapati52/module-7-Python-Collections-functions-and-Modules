696127bd3457f05ea347f4b51c4c960a34b4343f
piyushprajapati52 <prajapatipuso1976@gmail.com>
2025-08-08 09:09:28 +0530

    Update Access Elements by Index.py

--- module 7   Python – Collections, functions and Modules/practical/Accessing List/Access Elements by Index.py
+++ module 7   Python – Collections, functions and Modules/practical/Accessing List/Access Elements by Index.py
@@ -2,8 +2,9 @@
 fruits = ["apple","banana ","cherry","mango"]
 
 #accessing elements at different index positions
-print("Element at index 0:", fruits[0])  #First element
-print("Element at index 2:", fruits[2])  #First element
-print("Element at index -1:", fruits[-1])  #First element
-print("Element at index 3:", fruits[3])  #First element
-print("Element at index 1:", fruits[1])  #First element
+print("Element at index 0:", fruits[0])  
+print("Element at index 2:", fruits[2])  
+print("Element at index -1:", fruits[-1])  
+print("Element at index 3:", fruits[3])  
+print("Element at index 1:", fruits[1])  
+
